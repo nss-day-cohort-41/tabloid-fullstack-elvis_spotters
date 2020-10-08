72930d5645bd633a4c66311b19368276bae024a9
John Hester <hesterusmaximus@gmail.com>
2020-10-08 15:24:31 -0500

    added commment provider

--- Tabloid/client/src/providers/CommentProvider.js
+++ Tabloid/client/src/providers/CommentProvider.js
@@ -7,7 +7,23 @@ export const CommentContext = createContext();
 
 export const CommentProvider = (props) => {
 
+    const apiUrl = "api/comment";
     const [comments, setComments] = useState([]);
 
+    const getCommentsByPostId = (postId) => {
+        getToken().then((token) =>
+            fetch(apiUrl + `/${postId}`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((result) => result.json())
+                .then(setComments));
+    }
 
+    return (
+        <CommentContext.Provider value={{ comments, setComments, getCommentsByPostId }}>
+            {props.children}
+        </CommentContext.Provider>
+    );
 };
