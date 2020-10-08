011d65c7711c77bbafc54780f07444cf13ce71a1
David Larsen <davidplarsen@gmail.com>
2020-10-08 12:02:40 -0500

    Add post method in repo

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -97,6 +97,34 @@ namespace Tabloid.Repositories
             }
         }
 
+        public void Add(Post post)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Post (Title, Content, ImageLocation, CreateDateTime, PublishDateTime,
+                                          IsApproved, CategoryId, UserProfileId)
+                        OUTPUT INSERTED.ID
+                                  VALUES (@Title, @Content, @ImageLocation, CURRENT_TIMESTAMP, @PublishDateTime,
+                                          @IsApproved, @CategoryId, @UserProfileId);";
+
+                    DbUtils.AddParameter(cmd, "@Title", post.Title);
+                    DbUtils.AddParameter(cmd, "@Content", post.Content);
+                    DbUtils.AddParameter(cmd, "@ImageLocation", post.ImageLocation);
+                    DbUtils.AddParameter(cmd, "@CreateDateTime", post.CreateDateTime);
+                    DbUtils.AddParameter(cmd, "@PublishDateTime", post.PublishDateTime);
+                    DbUtils.AddParameter(cmd, "@IsApproved", post.IsApproved);
+                    DbUtils.AddParameter(cmd, "@CategoryId", post.CategoryId);
+                    DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
+
+                    post.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
         public void Update(Post post)
         {
             using (var conn = Connection)
