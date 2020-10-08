0d9b40e4fa8e6e7086366a6149bab23eb99dcd3f
David Larsen <davidplarsen@gmail.com>
2020-10-08 13:02:36 -0500

    Add post method in interface

--- Tabloid/Repositories/IPostRepository.cs
+++ Tabloid/Repositories/IPostRepository.cs
@@ -8,6 +8,7 @@ namespace Tabloid.Repositories
         List<Post> GetAllPosts();
         List<Post> GetAllPublishedPosts();
         Post GetPostById(int id);
+        void Add(Post post);
         void Update(Post post);
     }
 }
