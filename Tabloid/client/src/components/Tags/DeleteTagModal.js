f7b2e1b6be2087dff5fe0f9a19756342894e803f
James Su-Shum <sushumjames@gmail.com>
2020-10-09 16:04:01 -0400

    Created delete tag modal and corresponding methods on client side

---
+++ Tabloid/client/src/components/Tags/DeleteTagModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const DeleteTagModal = ({ deleteModal, deleteToggle, tagToDelete }) => {
+
+  return (
+    <div>
+      <Modal isOpen={deleteModal} toggle={deleteToggle}>
+        <ModalHeader toggle={deleteToggle}>Delete Tag</ModalHeader>
+        <ModalBody>
+          Are you sure you want to delete the <b>{tagToDelete.Name}</b> tag?
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={deleteToggle}>Cancel</Button>
+          <Button>Delete</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
+}
+
+export default DeleteTagModal;
--- Tabloid/client/src/components/Tags/Tag.js
+++ Tabloid/client/src/components/Tags/Tag.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ListGroupItem, Button } from 'reactstrap';
 
-const Tag = ({ tag }) => {
+const Tag = ({ tag, tagToBeDeleted }) => {
 
   return (
     <div>
       <ListGroupItem className="d-flex flex-row justify-content-between">
         {tag.name}
         <div>
-          <Button color="danger" className="mr-2">Delete</Button>
+          <Button color="danger" className="mr-2" onClick={() => tagToBeDeleted(tag)}>Delete</Button>
           <Button color="info">Edit</Button>
         </div>
       </ListGroupItem>
