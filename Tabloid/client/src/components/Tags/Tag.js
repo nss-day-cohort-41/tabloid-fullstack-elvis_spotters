da008ff758c184fada570d54339af502fdde0ab5
James Su-Shum <sushumjames@gmail.com>
2020-10-09 15:11:31 -0400

    Added delete and edit buttons to individual tags

--- Tabloid/client/src/components/Tags/Tag.js
+++ Tabloid/client/src/components/Tags/Tag.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { ListGroupItem } from 'reactstrap';
+import { ListGroupItem, Button } from 'reactstrap';
 
 const Tag = ({ tag }) => {
 
   return (
-    <ListGroupItem>{tag.name}</ListGroupItem>
+    <div>
+      <ListGroupItem className="d-flex flex-row justify-content-between">
+        {tag.name}
+        <div>
+          <Button color="danger" className="mr-2">Delete</Button>
+          <Button color="info">Edit</Button>
+        </div>
+      </ListGroupItem>
+    </div>
   )
 }
 
