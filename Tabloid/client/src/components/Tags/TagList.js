535343d6977829412991aa17b3f90f9deee96bbd
James Su-Shum <sushumjames@gmail.com>
2020-10-09 19:19:08 -0400

    Added title to tag management page

--- Tabloid/client/src/components/Tags/TagList.js
+++ Tabloid/client/src/components/Tags/TagList.js
@@ -46,6 +46,7 @@ const TagList = () => {
     <div>
       <CreateTagModal modal={modal} toggle={toggle} addTag={addTag} getAllTags={getAllTags} formFeedback={formFeedback} setFormFeedback={setFormFeedback} newTag={newTag} setNewTag={setNewTag} />
       <DeleteTagModal deleteModal={deleteModal} deleteToggle={deleteToggle} tagToDelete={tagToDelete} deleteTag={deleteTag} getAllTags={getAllTags} />
+      <h3 className="mb-4">Tag Management</h3>
       <Button className="mb-4" onClick={toggle}>Create Tag</Button>
       <ListGroup>
         {tagList.map(tag => <Tag key={tag.id} tag={tag} tagToBeDeleted={tagToBeDeleted} />)}
