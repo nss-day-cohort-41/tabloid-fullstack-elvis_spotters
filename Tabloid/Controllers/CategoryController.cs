cc79669c842eeb62ae65b4c2b5f087b30a373440
Adanieljohnson91 <anthonydanjohnson@gmail.com>
2020-10-09 15:03:28 -0500

    commiting to edit and create

--- Tabloid/Controllers/CategoryController.cs
+++ Tabloid/Controllers/CategoryController.cs
@@ -9,46 +9,45 @@ using Tabloid.Models;
 using Tabloid.Repositories;
 
 namespace Tabloid.Controllers
-
-{
-    [Authorize]
+    {
+   // [Authorize]
     [Route("api/[controller]")]
     [ApiController]
 
     public class CategoryController : ControllerBase
-    {
+        {
         private readonly ICategoryRepository _categoryRepository;
 
         public CategoryController(ICategoryRepository categoryRepository)
-        {
+            {
             _categoryRepository = categoryRepository;
-        }
+            }
 
         [HttpPost("create")]
         public IActionResult Post(Category category )
             {
             try
-            {
+                {
                 _categoryRepository.AddCategory(category);
                 return Ok();
-            }
-            catch (Exception ex)
-            {
+                }
+            catch(Exception ex)
+                {
                 return NotFound();
+                }
             }
-        }
-
         [HttpGet]
         public IActionResult GetAll()
-        {
-            try
             {
+            try
+                {
                 List<Category> categories = _categoryRepository.GetCategories();
                 return Ok(categories);
-            }
-            catch (Exception ex)
-            {
+                }
+            catch(Exception ex)
+                {
                 return NotFound();
+                }
             }
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
@@ -81,6 +80,4 @@ namespace Tabloid.Controllers
                 }
             }
         }
-
     }
-}
