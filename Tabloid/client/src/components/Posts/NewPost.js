ac635f603aaa1bfbd1bc9eda31c9a68a97542d12
David Larsen <davidplarsen@gmail.com>
2020-10-09 23:32:22 -0500

    Client-side form validation

--- Tabloid/client/src/components/Posts/NewPost.js
+++ Tabloid/client/src/components/Posts/NewPost.js
@@ -3,39 +3,56 @@ import React, { useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Container, Form, FormGroup, Col, Label, Input, Button, FormFeedback } from "reactstrap";
 import { PostContext } from "../../providers/PostProvider";
+import { ValidateNewPost } from "./ValidateNewPost";
 
 const NewPost = (props) => {
 
     const { saveNewPost, categories, getCategories } = useContext(PostContext);
 
     const [newPost, setNewPost] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
+    const [isFormValid, setIsFormValid] = useState(false);
+    const [formFeedback, setFormFeedback] = useState({
+        title: false,
+        content: false,
+        imageLocation: false,
+        category: false
+    });
 
     const history = useHistory();
 
     const handleFieldChange = (evt) => {
         const stateToUpdate = { ...newPost };
         stateToUpdate[evt.target.name] = evt.target.value;
+        const checkValidForm = ValidateNewPost(stateToUpdate);
+        setIsFormValid(checkValidForm.isValidated);
         setNewPost(stateToUpdate);
     }
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        setIsLoading(true);
+        const validation = ValidateNewPost(newPost);
+        setIsFormValid(validation.isValidated);
 
-        // Build object to send. Post is assigned the current userProfileId, date, and a "true" value to isApproved by the API.
-        const submittedPost = {
-            title: newPost.title,
-            content: newPost.content,
-            imageLocation: newPost.imageLocation || null,
-            publishDateTime: newPost.publishDateTime || null,
-            categoryId: parseInt(newPost.categoryId)
+        if (validation.isFormValid === false) {
+
+            const formFeedbackUpdate = { ...formFeedback };
+            formFeedbackUpdate[validation.field] = true;
+            setFormFeedback(formFeedbackUpdate);
+
+        } else {
+
+            // Build object to send. Post is assigned the current userProfileId, date, and a "true" value to isApproved by the API.
+            const submittedPost = {
+                title: newPost.title,
+                content: newPost.content,
+                imageLocation: newPost.imageLocation || null,
+                publishDateTime: newPost.publishDateTime || null,
+                categoryId: parseInt(newPost.categoryId)
+            }
+
+            saveNewPost(submittedPost)
+                .then(() => history.push("/"));
         }
-
-        console.log(submittedPost)
-
-        saveNewPost(submittedPost)
-            .then(() => history.push("/"));
     }
 
     useEffect(() => {
@@ -51,8 +68,8 @@ const NewPost = (props) => {
                 <FormGroup row>
                     <Label for="title" sm={2}>Title</Label>
                     <Col sm={10}>
-                        <Input type="text" name="title" id="title" placeholder="Title your post" maxLength="255" required
-                            onChange={handleFieldChange} />
+                        <Input type="text" name="title" id="title" placeholder="Title your post" maxLength="255"
+                            invalid={formFeedback.title} required onChange={handleFieldChange} />
                         <FormFeedback>Please give this post a title</FormFeedback>
                     </Col>
                 </FormGroup>
@@ -60,15 +77,15 @@ const NewPost = (props) => {
                     <Label for="content" sm={2}>Post Content</Label>
                     <Col sm={10}>
                         <Input type="textarea" name="content" id="content" placeholder="Say what you want to say here..." required
-                            onChange={handleFieldChange} />
-                        <FormFeedback>Every form needs content!</FormFeedback>
+                            invalid={formFeedback.content} onChange={handleFieldChange} />
+                        <FormFeedback>Every post needs content!</FormFeedback>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label for="imageLocation" sm={2}>Image URL (optional)</Label>
                     <Col sm={10}>
                         <Input type="url" name="imageLocation" id="imageLocation" placeholder="https://website.com/mypic" maxLength="255"
-                            onChange={handleFieldChange} />
+                            invalid={formFeedback.imageLocation} onChange={handleFieldChange} />
                         <FormFeedback>Please enter a proper URL or leave blank</FormFeedback>
                     </Col>
                 </FormGroup>
@@ -82,15 +99,17 @@ const NewPost = (props) => {
                 <FormGroup row>
                     <Label for="categoryId" sm={2}>Category</Label>
                     <Col sm={10}>
-                        <Input type="select" name="categoryId" id="categoryId" onChange={handleFieldChange} required>
+                        <Input type="select" name="categoryId" id="categoryId" invalid={formFeedback.category}
+                            onChange={handleFieldChange} required>
                             <option value="">Select a Category</option>
                             {categories.map(category =>
                                 <option key={category.id} value={category.id}>{category.name}</option>
                             )}
                         </Input>
+                        <FormFeedback>Please select a category</FormFeedback>
                     </Col>
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button disabled={!isFormValid}>Submit</Button>
             </Form>
         </Container>
     )
---
+++ Tabloid/client/src/components/Posts/ValidateNewPost.js
@@ -0,0 +1,32 @@
+export function ValidateNewPost(post) {
+    /* All validations return an object. First key is the boolean validation itself.
+        Second key is the unvalidated field. */
+
+    // Validate title
+    if (!post.title || post.title.trim() === "") {
+        return { isValidated: false, field: "title" }
+    }
+
+    // Validate content
+    if (!post.content || post.content.trim() === "") {
+        return { isValidated: false, field: "content" };
+    }
+
+    // Validate imageLocation, if present
+    if (post.imageLocation) {
+        try {
+            new URL(post.imageLocation)
+        } catch (_) {
+            return { isValidated: false, field: "imageLocation" };
+        }
+    }
+
+    // Validate category
+    if (!post.categoryId) {
+        return { isValidated: false, field: "category" };
+    }
+
+    // All validations pass
+    return { isValidated: true, field: "" };
+
+}
