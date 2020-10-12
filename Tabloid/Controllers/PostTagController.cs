d582a2f6b0045342361e3b3b265536246c2c414c
James Su-Shum <sushumjames@gmail.com>
2020-10-12 16:50:33 -0400

    Added Authorization attribute to PostTagController

---
+++ Tabloid/Controllers/PostTagController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostTagController : ControllerBase
+    {
+        private readonly IPostTagRepository _postTagRepository;
+
+        public PostTagController(IPostTagRepository postTagRepository)
+        {
+            _postTagRepository = postTagRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var postTags = _postTagRepository.GetTagsByPostId(id);
+            if (postTags.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(postTags);
+        }
+    }
+}
