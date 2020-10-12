6ce8b5818a640f69746ba998e968a09e2c83cd5e
Adanieljohnson91 <anthonydanjohnson@gmail.com>
2020-10-12 15:44:24 -0500

    userprofile list

--- Tabloid/client/src/components/UserProfile/UserProfiles.jsx
+++ Tabloid/client/src/components/UserProfile/UserProfiles.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {ProfileContext} from "../../providers/ProfileProvider";
+import ProfileListItems from "./UserProfileListItem";
 
 const UserProfiles = () =>{
     const [userProfiles, setUserProfiles] = React.useState([]);
 
-    const {getUserProfiles, ProfileList} = React.useContext(ProfileContext);
+    const {getUserProfiles} = React.useContext(ProfileContext);
 
     const getProfiles = async () =>{
        let user = await getUserProfiles();
-      await setUserProfiles(user)
-       console.log(ProfileList, "Profile List", user, userProfiles);
+      await setUserProfiles(user.map((ele, index)=>{
+          return <ProfileListItems key={index} user={ele} index={index+1}/>
+      }))
     }
 
 
@@ -18,7 +20,24 @@ const UserProfiles = () =>{
     }, [])
 
     return (
-    <div>{userProfiles.length > 0 ? <h1>Working!!!</h1> : <small>Not Working</small>}</div>
+    <div>
+        <h1>User Profiles</h1>
+        {userProfiles.length > 0 ?  <table className="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name </th>
+        <th scope="col">Username</th>
+        <th scope="col">Type</th>
+      </tr>
+    </thead>
+    <tbody>
+    {userProfiles}
+    </tbody>
+  </table> : <small>Not Working</small>}
+   
+
+    </div>
     )
 }
 
