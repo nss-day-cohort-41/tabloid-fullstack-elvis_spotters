4721ee6858396c6a57fde88470e9ea866b92ce7e
David Larsen <davidplarsen@gmail.com>
2020-10-20 16:03:19 -0500

    Make 404 error more appropriate for presentation

--- Tabloid/client/src/components/NotFound.jsx
+++ Tabloid/client/src/components/NotFound.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 
 
-export default () =>{
+export default () => {
     return (
         <>
-        <div className="container-fluid text-center">
-            <h3>! 404 !</h3>
-        <h1>Suck it Nerd</h1>
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
-        </div>
+            <div className="container-fluid text-center">
+                <h3>! 404 !</h3>
+                <h1>Not Found</h1>
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+                <img src="https://media4.giphy.com/media/dwZU5BarQHmla/giphy.gif" alt="elvis" />
+            </div>
         </>
     )
 }
